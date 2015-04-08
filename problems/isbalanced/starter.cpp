09c638403e77452be5f668775aad0bbe917fc394
Varun Ramesh <varunramesh@live.com>
2015-04-08 03:27:21 -0700

    Some basic test cases.

--- problems/isbalanced/starter.cpp
+++ problems/isbalanced/starter.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+using namespace std;
+
+struct node {
+	int value;
+	node* left;
+	node* right;
+};
+bool isbalanced(node* root);
+
+node* readTree() {
+	node* n = new node();
+	n->value = 0;
+	n->left = NULL;
+	n->right = NULL;
+}
 
 int main() {
+	node* tree = readTree();
+	cout << boolalpha << isbalanced(tree) << noboolalpha;
+}
+
+// Return whether or not the tree is balanced.
+bool isbalanced(node* root) {
+	return true;
 }
